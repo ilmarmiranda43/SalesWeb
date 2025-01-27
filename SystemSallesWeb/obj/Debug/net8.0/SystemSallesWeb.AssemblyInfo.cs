4d97b3e2736bbe3137a7b8edbb2fe085c6b78c9e
ilmarmiranda43 <ilmarmiranda43@gmail.com>
2025-01-27 19:36:41 -0300

    Deleting Department view and controller

--- SystemSallesWeb/obj/Debug/net8.0/SystemSallesWeb.AssemblyInfo.cs
+++ SystemSallesWeb/obj/Debug/net8.0/SystemSallesWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SystemSallesWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b5bc8303fcb7da3df70659acb9bf93eeea723de")]
 [assembly: System.Reflection.AssemblyProductAttribute("SystemSallesWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SystemSallesWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
