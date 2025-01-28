722b305d463a8ef942e552a0653cd8df759e8243
ilmarmiranda43 <ilmarmiranda43@gmail.com>
2025-01-28 00:43:48 -0300

    SellersController

--- SystemSallesWeb/obj/Debug/net8.0/SystemSallesWeb.AssemblyInfo.cs
+++ SystemSallesWeb/obj/Debug/net8.0/SystemSallesWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SystemSallesWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17441c2cbd43c4d4e6764d0a086321d891d3d720")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e6417997cd6f513db1a36879ccb77b42591896e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SystemSallesWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SystemSallesWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
